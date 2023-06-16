042b3e667cfa6bf66211acf3bd7e51a279ebe2f3
chandrikathota <chandrikathota1005@gmail.com>
2023-06-16 12:43:00 +0530

    naming commit

--- addressbook.js
+++ addressbook.js
@@ -1,229 +1,214 @@
 
-function openpopup(){
+var buttonContainer = document.getElementById('buttonContainer');
+for (var i = 65; i <= 90; i++) {
+    var letter = String.fromCharCode(i);
+    var button = document.createElement('button');
+    button.type = "button"
+    button.innerText = letter;
+    button.id = letter;
+    button.classList.add('letter-button');
+    (function(button) {
+        button.onclick = function() {
+        alphaButtonClicked(button.id);
+        };
+    })(button);
+    buttonContainer.appendChild(button);
+    
+}
+
+function openPopup(){
     document.getElementById("addemp-popup").style.display="flex";
 }
-function closepopup(){
+function closePopup(){
     document.getElementById("addemp-popup").style.display="none";
 }
-function clearsearchbar(){
+function clearSearchBar(){
     document.getElementById("search").value="";
 }
 
 
-let employees=[]
-const emp1={
-    firstname:"John",
-    lastname:"Andrews",
-    preferedname:"John Andrews",
-    email:"john.a@gmail.com",
-    location:"Boston",
-    job:"Recruiting Expert",
-    department:"Human Resources",
-    skypeid:"345671"
-}
-employees.push(emp1);
-const emp2={
-    firstname:"Lea",
-    lastname:"Dileali",
-    preferedname:"Lea Dileali",
-    email:"lea.d@gmail.com",
-    location:"Seattle",
-    job:".Net developer",
-    department:"IT department",
-    skypeid:"236849"
-}
-employees.push(emp2);
-const emp3={
-    firstname:"Shaun",
-    lastname:"Murphy",
-    preferedname:"Shaun Murphy",
-    email:"shaun@gmail.com",
-    location:"Seattle",
-    job:"Product Manager",
-    department:"IT department",
-    skypeid:"346721"
-}
-employees.push(emp3);
-const emp4={
-    firstname:"Audrea",
-    lastname:"Lim",
-    preferedname:"Audrea Lim",
-    email:"audrealim@gmail.com",
-    location:"Seattle",
-    job:"System Analyst",
-    department:"IT department",
-    skypeid:"563728"
-}
-employees.push(emp4);
-const emp5={
-    firstname:"Neil",
-    lastname:"Melendez",
-    preferedname:"Neil Melendez",
-    email:"melendez@gmail.com",
-    location:"Portland",
-    job:"Data Analyst",
-    department:"IT department",
-    skypeid:"890123"
-}
-employees.push(emp5);
-const emp6={
-    firstname:"Aaron",
-    lastname:"Glassman",
-    preferedname:"Aaron Glassman",
-    email:"dr.glassman@gmail.com",
-    location:"Boston",
-    job:"Chief Marketing Officer",
-    department:"Marketing department",
-    skypeid:"781234"
-}
-employees.push(emp6);
-const emp7={
-    firstname:"Radhika",
-    lastname:"Murthy",
-    preferedname:"Radhika Murthy",
-    email:"mradhika@gmail.com",
-    location:"Chicago",
-    job:"Product Manager",
-    department:"IT department",
-    skypeid:"127801"
-}
-employees.push(emp7);
-const emp8={
-    firstname:"James",
-    lastname:"Robert",
-    preferedname:"James Robert",
-    email:"jrobert@gmail.com",
-    location:"Chicago",
-    job:"Product Manager",
-    department:"IT department",
-    skypeid:"818132"
-}
-employees.push(emp8);
-const emp9={
-    firstname:"Michael",
-    lastname:"Anderson",
-    preferedname:"Michael Anderson",
-    email:"amichael@gmail.com",
-    location:"Portland",
-    job:"Junior Software Engineer",
-    department:"IT department",
-    skypeid:"567183"
-}
-employees.push(emp9);
-const emp10={
-    firstname:"David",
-    lastname:"Miller",
-    preferedname:"David Miller",
-    email:"dmiller@gmail.com",
-    location:"Boston",
-    job:"Marketing Analyst",
-    department:"Marketing department",
-    skypeid:"128904"
-}
-employees.push(emp10);
+let employees=[
+    {
+        firstName:"John",
+        lastName:"Andrews",
+        preferedName:"John Andrews",
+        email:"john.a@gmail.com",
+        location:"Boston",
+        job:"Recruiting Expert",
+        department:"Human Resources",
+        skypeid:"345671"
+    },
+    {
+        firstName:"Lea",
+        lastName:"Dileali",
+        preferedName:"Lea Dileali",
+        email:"lea.d@gmail.com",
+        location:"Seattle",
+        job:".Net developer",
+        department:"IT department",
+        skypeid:"236849"
+    },
+    {
+        firstName:"Shaun",
+        lastName:"Murphy",
+        preferedName:"Shaun Murphy",
+        email:"shaun@gmail.com",
+        location:"Seattle",
+        job:"Product Manager",
+        department:"IT department",
+        skypeid:"346721"
+    },
+    {
+        firstName:"Audrea",
+        lastName:"Lim",
+        preferedName:"Audrea Lim",
+        email:"audrealim@gmail.com",
+        location:"Seattle",
+        job:"System Analyst",
+        department:"IT department",
+        skypeid:"563728"
+    },
+    {
+        firstName:"Neil",
+        lastName:"Melendez",
+        preferedName:"Neil Melendez",
+        email:"melendez@gmail.com",
+        location:"Portland",
+        job:"Data Analyst",
+        department:"IT department",
+        skypeid:"890123"
+    },
+    {
+        firstName:"Aaron",
+        lastName:"Glassman",
+        preferedName:"Aaron Glassman",
+        email:"dr.glassman@gmail.com",
+        location:"Boston",
+        job:"Chief Marketing Officer",
+        department:"Marketing department",
+        skypeid:"781234"
+    },
+    {
+        firstName:"Radhika",
+        lastName:"Murthy",
+        preferedName:"Radhika Murthy",
+        email:"mradhika@gmail.com",
+        location:"Chicago",
+        job:"Product Manager",
+        department:"IT department",
+        skypeid:"127801"
+    },
+    {
+        firstName:"James",
+        lastName:"Robert",
+        preferedName:"James Robert",
+        email:"jrobert@gmail.com",
+        location:"Chicago",
+        job:"Product Manager",
+        department:"IT department",
+        skypeid:"818132"
+    },
+    {
+        firstName:"Michael",
+        lastName:"Anderson",
+        preferedName:"Michael Anderson",
+        email:"amichael@gmail.com",
+        location:"Portland",
+        job:"Junior Software Engineer",
+        department:"IT department",
+        skypeid:"567183"
+    },
+    {
+        firstName:"David",
+        lastName:"Miller",
+        preferedName:"David Miller",
+        email:"dmiller@gmail.com",
+        location:"Boston",
+        job:"Marketing Analyst",
+        department:"Marketing department",
+        skypeid:"128904"
+    }
 
-
-console.log(document.getElementById("profile-set").innerHTML)
+]
 
 
 // searchbar 
 
-let preferednames=[]
-for(let i=0;i<employees.length;i++){
-    preferednames.push(employees[i].preferedname)
-}
-let jobtitles=[]
-for(let i=0;i<employees.length;i++){
-    jobtitles.push(employees[i].job)
-}
-let departments=[]
-for(let i=0;i<employees.length;i++){
-    departments.push(employees[i].department)
-}
-let offices=[]
-for(let i=0;i<employees.length;i++){
-    offices.push(employees[i].location)
-}
-let skypeids=[]
-for(let i=0;i<employees.length;i++){
-    skypeids.push(employees[i].skypeid)
-}
+
+let searchBar=document.getElementById("search");
+var searchElement=" "
+
+let filterByElement=document.getElementById("filterby-option")
 
 
-let searchbar=document.getElementById("search");
-var searchelement=" "
-
-let filterbyelement=document.getElementById("filterby-option")
-
-
-filterbyelement.addEventListener("change",function(){
-    var filterbyvalue=filterbyelement.value;
-    retrievesearchelement(filterbyvalue)
+filterByElement.addEventListener("change",function(){
+    var filterByValue=filterByElement.value;
+    retrieveSearchElement(filterByValue)
 })
 
 
-function retrievesearchelement(filterbyvalue){
-    searchbar.addEventListener("input",function(event){
+function retrieveSearchElement(filterbyvalue){
+    searchBar.addEventListener("input",function(event){
         event.preventDefault()
-        searchelement=event.target.value;
-        performsearch(searchelement,filterbyvalue)
+        searchElement=event.target.value;
+        performSearch(searchElement,filterByValue)
     })
 }
 
 
-function performsearch(searchelement,filterbyvalue){
-    console.log(searchelement)
-    console.log(filterbyvalue)
+function performSearch(searchElement,filterByValue){
+    console.log(searchElement)
+    console.log(filterByValue)
     console.log(profiles)
-    let searchingelement=searchelement.toUpperCase();
-    if(filterbyvalue=="Preferred Name" && searchingelement !=""){
+    let searchingElement=searchElement.toUpperCase();
+    if(filterByValue=="Preferred Name" && searchingElement !=""){
         let profiles=[]
         for(let i=0;i<employees.length;i++){
-            if((employees[i].preferedname.toUpperCase()).includes(searchingelement.toString())){
+            if((employees[i].preferedName.toUpperCase()).includes(searchingElement.toString())){
                 profiles.push(employees[i])
             }
         }
-        displayemployeedetails(profiles)
+        displayEmployeeDetails(profiles)
     }
 
-    if(filterbyvalue=="Job Title" && searchingelement!=""){
+    if(filterByValue=="Job Title" && searchingElement!=""){
         let profiles=[]
         for(let i=0;i<employees.length;i++){
-            if(employees[i].job.toUpperCase().includes(searchingelement.toString())){
+            if(employees[i].job.toUpperCase().includes(searchingElement.toString())){
                 profiles.push(employees[i])
             }
         }
-        displayemployeedetails(profiles)
+        displayEmployeeDetails(profiles)
     }
     
-    if(filterbyvalue=="Office" && searchingelement!=""){
+    if(filterByValue=="Office" && searchingElement!=""){
         let profiles=[]
         for(let i=0;i<employees.length;i++){
-            if(employees[i].location.toUpperCase().includes(searchingelement.toString())){
+            if(employees[i].location.toUpperCase().includes(searchingElement.toString())){
                 profiles.push(employees[i])
             }
         }
-        displayemployeedetails(profiles)
+        displayEmployeeDetails(profiles)
     }
 
-    if(filterbyvalue=="Department" && searchingelement!=""){
+    if(filterByValue=="Department" && searchingElement!=""){
         let profiles=[]
         for(let i=0;i<employees.length;i++){
-            if(employees[i].department.toUpperCase().includes(searchingelement.toString())){
+            if(employees[i].department.toUpperCase().includes(searchingElement.toString())){
                 profiles.push(employees[i])
             }
         }
-        displayemployeedetails(profiles)
+        displayEmployeeDetails(profiles)
     }
 
-    if(filterbyvalue=="Skype ID" && searchingelement!=""){
+    if(filterByValue=="Skype ID" && searchingElement!=""){
         let profiles=[]
         for(let i=0;i<employees.length;i++){
-            if(employees[i].skypeid .includes(searchelement)){
+            if(employees[i].skypeid .includes(searchElement)){
                 profiles.push(employees[i])
             }
         }
-        displayemployeedetails(profiles)
+        displayEmployeeDetails(profiles)
     }
 
 }
@@ -232,45 +217,45 @@ function performsearch(searchelement,filterbyvalue){
 
 profiles=[]
 
-departmentfilters=[]
+departmentFilters=[]
 for (let i=0;i<employees.length;i++){
     let deptli=document.createElement('li');
     deptli.className="dept-filter";
     deptli.id=employees[i].department;
     
 
-    if(!departmentfilters.includes(employees[i].department)){
-        departmentfilters.push(employees[i].department);
+    if(!departmentFilters.includes(employees[i].department)){
+        departmentFilters.push(employees[i].department);
         deptli.innerText= employees[i].department;
         deptli.addEventListener("click", handleFilterClick);
         document.getElementById("depart-filters").appendChild(deptli);
     }  
 }
 
-locationfilters=[]
+locationFilters=[]
 for(let i=0;i<employees.length;i++){
     let officeli=document.createElement('li');
     officeli.className="off-filters";
     officeli.id=employees[i].location;
     
     
-    if(!locationfilters.includes(employees[i].location)){
-        locationfilters.push(employees[i].location);
+    if(!locationFilters.includes(employees[i].location)){
+        locationFilters.push(employees[i].location);
         officeli.innerText=employees[i].location;
         officeli.addEventListener("click", handleFilterClick);
         document.getElementById("offc-filters").appendChild(officeli);
     }
 }
 
-jobtitlefilters=[]
+jobTitleFilters=[]
 for(let i=0;i<employees.length;i++){
     let jobli=document.createElement('li');
     jobli.className="jobtle-filters";
     jobli.id=employees[i].job;
     
 
-    if(!jobtitlefilters.includes(employees[i].job)){
-        jobtitlefilters.push(employees[i].job);
+    if(!jobTitleFilters.includes(employees[i].job)){
+        jobTitleFilters.push(employees[i].job);
         jobli.innerText=employees[i].job;
         jobli.addEventListener("click", handleFilterClick);
         document.getElementById("jobtle-filters").appendChild(jobli);
@@ -279,46 +264,46 @@ for(let i=0;i<employees.length;i++){
 }
 
 function handleFilterClick(event) {
-    const filter = event.target.id;
-    getfilteredprofiles(filter)
+    const filterId = event.target.id;
+    getfilteredprofiles(filterId)
 }
 
-function getfilteredprofiles(filterid){
+function getfilteredprofiles(filterId){
     let profiles=[]
     employees.forEach(emp=>{
         const location=emp.location;
         const job=emp.job;
         const department=emp.department;
 
-        if(location.includes(filterid) || job.includes(filterid) || department.includes(filterid)){
+        if(location.includes(filterId) || job.includes(filterId) || department.includes(filterId)){
             profiles.push(emp)
         }
     })
-    displayemployeedetails(profiles)
+    displayEmployeeDetails(profiles)
 }
 
 //all-employees display
 
 document.getElementById("all-btn").addEventListener("click",function(){
-    displayemployeedetails(employees);
+    displayEmployeeDetails(employees);
 })
 
 
 //alpha buttons
 
-function handleclickedbutton(buttonid){
+function alphaButtonClicked(buttonid){
     let clickedAlphaButton=document.getElementById(buttonid).innerHTML;
     let profiles=[]
     for(let i=0;i<employees.length;i++){
-        if(employees[i].preferedname[0]==clickedAlphaButton){
+        if(employees[i].preferedName[0]==clickedAlphaButton){
             profiles.push(employees[i])
         }
     }
-    displayemployeedetails(profiles)
+    displayEmployeeDetails(profiles)
 }
 
 
-function displayemployeedetails(profiles){
+function displayEmployeeDetails(profiles){
 
     document.getElementById("profile-set").innerHTML = '';
         profiles.forEach(prof=>{
@@ -326,7 +311,7 @@ function displayemployeedetails(profiles){
             profile.className="profile";
             
             let profile_img=document.createElement("img");
-            profile_img.src="assets/emp1.png";
+            profile_img.src="assets/employee.png";
         
             let emp_details=document.createElement("div");
             emp_details.className="emp-details";
@@ -340,7 +325,7 @@ function displayemployeedetails(profiles){
             emp_name.className="emp-name";
             emp_job.className="emp-job";
             emp_dept.className="emp-dept";
-            emp_name.innerHTML=prof.preferedname;
+            emp_name.innerHTML=prof.preferedName;
             emp_job.innerHTML=prof.job;
             emp_dept.innerHTML=prof.department;
             
@@ -372,14 +357,13 @@ function displayemployeedetails(profiles){
             icons.appendChild(heart_icon)
 
             document.getElementById("profile-set").appendChild(profile)
-            console.log(document.getElementById("profile-set").innerHTML==null)
         })
               
             
 }
 
 
-function formvalidation(){
+function formValidation(){
     var firstName=document.employeeform.fstname.value;
     var lastName =document.employeeform.lstname.value;
     var email=document.employeeform.email.value;
@@ -388,32 +372,34 @@ function formvalidation(){
     var department=document.employeeform.deptname.value;
     var skypeId=document.employeeform.skypeid.value;
 
-    if(firstName==""){
-        document.getElementById("fstnamelbl").style.color="red";
-        document.getElementById("fstnamelbl").innerHTML="Please enter FirstName *";
-    }
-    if(lastName==""){
-        document.getElementById("lastnamelbl").style.color="red";
-        document.getElementById("lastnamelbl").innerHTML="Please enter LastName *";
-    }
-    if(email==""){
-        document.getElementById("emaillbl").style.color="red";
-        document.getElementById("emaillbl").innerHTML="Please enter Email *";
-    }
-    if(jobTitle==""){
-        document.getElementById("jobtitlelbl").style.color="red";
-        document.getElementById("jobtitlelbl").innerHTML="Please enter Job Title *";
-    }
-    if(location==""){
-        document.getElementById("offclbl").style.color="red";
-        document.getElementById("offclbl").innerHTML="Please enter Office *";
-    }
-    if(department==""){
-        document.getElementById("deptlbl").style.color="red";
-        document.getElementById("deptlbl").innerHTML="Please enter Department *";
-    }
-    if(skypeId==""){
-        document.getElementById("skypelbl").style.color="red";
-        document.getElementById("skypelbl").innerHTML="Please enter Skype Id *";
-    }
+    // if(firstName==""){
+    //     document.getElementById("fstnamelbl").style.color="red";
+    //     document.getElementById("fstnamelbl").innerHTML="Please enter FirstName *";
+    // }
+    // if(lastName==""){
+    //     document.getElementById("lastnamelbl").style.color="red";
+    //     document.getElementById("lastnamelbl").innerHTML="Please enter LastName *";
+    // }
+    // if(email==""){
+    //     document.getElementById("emaillbl").style.color="red";
+    //     document.getElementById("emaillbl").innerHTML="Please enter Email *";
+    // }
+    // if(jobTitle==""){
+    //     document.getElementById("jobtitlelbl").style.color="red";
+    //     document.getElementById("jobtitlelbl").innerHTML="Please enter Job Title *";
+    // }
+    // if(location==""){
+    //     document.getElementById("offclbl").style.color="red";
+    //     document.getElementById("offclbl").innerHTML="Please enter Office *";
+    // }
+    // if(department==""){
+    //     document.getElementById("deptlbl").style.color="red";
+    //     document.getElementById("deptlbl").innerHTML="Please enter Department *";
+    // }
+    // if(skypeId==""){
+    //     document.getElementById("skypelbl").style.color="red";
+    //     document.getElementById("skypelbl").innerHTML="Please enter Skype Id *";
+    // }
+
+
 }
